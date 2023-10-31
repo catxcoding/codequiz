956cd8aefff753afaf0fdd662ae92020da11db83
catxcoding <145952390+catxcoding@users.noreply.github.com>
2023-10-30 18:24:23 -0700

    Started README, Added Quiz Questions, Edited everything

---
+++ Assets/script.js
@@ -0,0 +1,284 @@
+// script.js 
+
+let questions = [ 
+	{ 
+		prompt: `What tag is used to specify a section of text that has been quoted from another source?`, 
+		options: [ 
+			"<strong>", 
+			"<em>", 
+			"<a>", 
+			"<blockquote>", 
+		], 
+		answer: "<blockquote>", 
+	}, 
+
+	{ 
+		prompt: `What tag is used to define a list item (in a bulleted list)?`, 
+		options: [ 
+			"<li>", 
+			"<u>>", 
+			"<ul>", 
+			"<s>", 
+		], 
+		answer: "ul", 
+	}, 
+
+	{ 
+		prompt: `In JavaScript, what element is used to store and manipulate text usually in multiples?`, 
+		options: [ 
+			"Arrays", 
+			"Function", 
+			"Variables", 
+			"Strings", 
+		], 
+		answer: "Strings", 
+	}, 
+
+	{ 
+		prompt: `In JavaScript, which 
+				of the following is 
+				a logical operator?`, 
+		options: ["|", "&&", "%", "/"], 
+		answer: "&&", 
+	}, 
+
+	{ 
+		prompt: `Where is the JavaScript placed inside an HTML document or page?`, 
+		options: [ 
+			"In the <footer> section", 
+			"In the <title> section", 
+			"In the <meta> section", 
+			"In the <body> and <head> sections", 
+		], 
+		answer: "In the <body> and <head> sections", 
+	}, 
+
+    { 
+		prompt: `CSS stands for ____ Style Sheets.`, 
+		options: [ 
+			"Curious", 
+			"Concave", 
+			"Cascading", 
+			"Concept", 
+		], 
+		answer: "Cascading", 
+	}, 
+
+    { 
+		prompt: `What is the CSS property that sets the size of the whitespace outside the borders of the content?`, 
+		options: [ 
+			"Spacer", 
+			"Block-level", 
+			"Margin", 
+			"Line", 
+		], 
+		answer: "Margin", 
+	}, 
+]; 
+
+// Get Dom Elements 
+
+let questionsEl = 
+	document.querySelector( 
+		"#questions"
+	); 
+let timerEl = 
+	document.querySelector("#timer"); 
+let choicesEl = 
+	document.querySelector("#options"); 
+let submitBtn = document.querySelector( 
+	"#submit-score"
+); 
+let startBtn = 
+	document.querySelector("#start"); 
+let nameEl = 
+	document.querySelector("#name"); 
+let feedbackEl = document.querySelector( 
+	"#feedback"
+); 
+let reStartBtn = 
+	document.querySelector("#restart"); 
+
+// Quiz's initial state 
+let currentQuestionIndex = 0; 
+let time = questions.length * 15; 
+let timerId; 
+
+// Start quiz and hide frontpage 
+
+function quizStart() { 
+	timerId = setInterval( 
+		clockTick, 
+		1000 
+	); 
+	timerEl.textContent = time; 
+	let landingScreenEl = 
+		document.getElementById( 
+			"start-screen"
+		); 
+	landingScreenEl.setAttribute( 
+		"class", 
+		"hide"
+	); 
+	questionsEl.removeAttribute( 
+		"class"
+	); 
+	getQuestion(); 
+} 
+
+// Loop through array of questions and 
+// Answers and create list with buttons 
+function getQuestion() { 
+	let currentQuestion = 
+		questions[currentQuestionIndex]; 
+	let promptEl = 
+		document.getElementById( 
+			"question-words"
+		); 
+	promptEl.textContent = 
+		currentQuestion.prompt; 
+	choicesEl.innerHTML = ""; 
+	currentQuestion.options.forEach( 
+		function (choice, i) { 
+			let choiceBtn = 
+				document.createElement( 
+					"button"
+				); 
+			choiceBtn.setAttribute( 
+				"value", 
+				choice 
+			); 
+			choiceBtn.textContent = 
+				i + 1 + ". " + choice; 
+			choiceBtn.onclick = 
+				questionClick; 
+			choicesEl.appendChild( 
+				choiceBtn 
+			); 
+		} 
+	); 
+} 
+
+// Check for right answers and deduct 
+// Time for wrong answer, go to next question 
+
+function questionClick() { 
+	if ( 
+		this.value !== 
+		questions[currentQuestionIndex] 
+			.answer 
+	) { 
+		time -= 10; 
+		if (time < 0) { 
+			time = 0; 
+		} 
+		timerEl.textContent = time; 
+		feedbackEl.textContent = `Wrong! The correct answer was 
+		${questions[currentQuestionIndex].answer}.`; 
+		feedbackEl.style.color = "red"; 
+	} else { 
+		feedbackEl.textContent = 
+			"Correct!"; 
+		feedbackEl.style.color = 
+			"green"; 
+	} 
+	feedbackEl.setAttribute( 
+		"class", 
+		"feedback"
+	); 
+	setTimeout(function () { 
+		feedbackEl.setAttribute( 
+			"class", 
+			"feedback hide"
+		); 
+	}, 2000); 
+	currentQuestionIndex++; 
+	if ( 
+		currentQuestionIndex === 
+		questions.length 
+	) { 
+		quizEnd(); 
+	} else { 
+		getQuestion(); 
+	} 
+} 
+
+// End quiz by hiding questions, 
+// Stop timer and show final score 
+
+function quizEnd() { 
+	clearInterval(timerId); 
+	let endScreenEl = 
+		document.getElementById( 
+			"quiz-end"
+		); 
+	endScreenEl.removeAttribute( 
+		"class"
+	); 
+	let finalScoreEl = 
+		document.getElementById( 
+			"score-final"
+		); 
+	finalScoreEl.textContent = time; 
+	questionsEl.setAttribute( 
+		"class", 
+		"hide"
+	); 
+} 
+
+// End quiz if timer reaches 0 
+
+function clockTick() { 
+	time--; 
+	timerEl.textContent = time; 
+	if (time <= 0) { 
+		quizEnd(); 
+	} 
+} 
+
+// Save score in local storage 
+// Along with users' name 
+
+function saveHighscore() { 
+	let name = nameEl.value.trim(); 
+	if (name !== "") { 
+		let highscores = 
+			JSON.parse( 
+				window.localStorage.getItem( 
+					"highscores"
+				) 
+			) || []; 
+		let newScore = { 
+			score: time, 
+			name: name, 
+		}; 
+		highscores.push(newScore); 
+		window.localStorage.setItem( 
+			"highscores", 
+			JSON.stringify(highscores) 
+		); 
+		alert( 
+			"Your Score has been Submitted"
+		); 
+	} 
+} 
+
+// Save users' score after pressing enter 
+
+function checkForEnter(event) { 
+	if (event.key === "Enter") { 
+		saveHighscore(); 
+		alert( 
+			"Your Score has been Submitted"
+		); 
+	} 
+} 
+nameEl.onkeyup = checkForEnter; 
+
+// Save users' score after clicking submit 
+
+submitBtn.onclick = saveHighscore; 
+
+// Start quiz after clicking start quiz 
+
+startBtn.onclick = quizStart;
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # codequiz
 04 Web APIs: Code Quiz
+
+## Description
+
+
+## User Story via Client
+
+
+
+## Acceptance Criteria via Client
+
+
+
+
+### Links
+
+
+[Deployed Site](https://catxcoding.github.io/codequiz)
+
+[Repository](https://github.com/catxcoding/codequiz)
+
+![Screenshot](screenshot.png)
+
+
+
+### Resources Used
+
+```
+* Xpert Leaning Assistant
+* Office Hours before/after class
+* Quiz Questions: https://www.codeconquest.com/coding-quizzes/
+* Geeks For Geeks Website
+* W3Schools
+* MDN
+```
