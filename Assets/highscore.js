b0ae25044f76d2c9843f577985d365e2ea54edc6
catxcoding <145952390+catxcoding@users.noreply.github.com>
2023-10-30 21:01:31 -0700

    FInal edits

--- Assets/highscore.js
+++ Assets/highscore.js
@@ -8,43 +8,29 @@ let scoresBtn = document.querySelector(
 // Retrieving scores from localStorage 
 
 function printHighscores() { 
-	let highscores = 
-		JSON.parse( 
-			window.localStorage.getItem( 
-				"highscores"
-			) 
-		) || []; 
-	highscores.sort(function (a, b) { 
-		return b.score - a.score; 
-	}); 
-	highscores.forEach(function ( 
-		score 
-	) { 
-		let liTag = 
-			document.createElement( 
-				"li"
-			); 
-		liTag.textContent = 
-			score.name + 
-			" - " + 
-			score.score; 
-		let olEl = 
-			document.getElementById( 
-				"highscores"
-			); 
-		olEl.appendChild(liTag); 
-	}); 
-} 
+    let highscores = JSON.parse(localStorage.getItem("highscores")) || []; 
+    highscores.sort(function (a, b) { 
+        return b.score - a.score; 
+    }); 
+
+    let highscoresList = document.getElementById("highscoresList");
+
+    highscores.forEach(function (score) { 
+        let liTag = document.createElement("li"); 
+        liTag.textContent = score.name + " - " + score.score; 
+
+        highscoresList.appendChild(liTag);
+    });
+}
 
 // Clear previous scores when users click clear 
 function clearHighscores() { 
-	window.localStorage.removeItem( 
-		"highscores"
-	); 
-	window.location.reload(); 
+    window.localStorage.removeItem( 
+        "highscores"
+    ); 
+    window.location.reload(); 
 } 
 document.getElementById( 
-	"clear"
+    "clear"
 ).onclick = clearHighscores; 
-
 printHighscores();
